469a329b2dd96c0307e4049fd7af863ed6dec6f5
Advikjohari12 <advikjohari39@gmail.com>
2026-09-04 15:18:04 +0530

    Initial commit

---
+++ SubarraySumK.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class SubarraySumK {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size: ");
+        int n = sc.nextInt();
+
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        System.out.print("Enter k: ");
+        int k = sc.nextInt();
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, 1);
+
+        int sum = 0;
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += nums[i];
+
+            if (map.containsKey(sum - k)) {
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        System.out.println("Number of Subarrays: " + count);
+    }
+}
