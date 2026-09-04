0fec403951fc509e19f39f1111e41ae216f497f4
Advikjohari12 <advikjohari39@gmail.com>
2026-09-04 11:52:26 +0530

    Added Java programs

---
+++ WordReverse.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class WordReverse {
+
+    static String reverseEachWord(String sentence) {
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder(word);
+            result = result + sb.reverse() + " ";
+        }
+
+        return result.trim();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter sentence: ");
+        String sentence = sc.nextLine();
+
+        System.out.println(reverseEachWord(sentence));
+    }
+}
